2aebe1b74a63ef7c92688b30f52cc178ea7b2052
Jonas Rademar <jonas.rademar@gmail.com>
2013-01-31 05:46:37 +0000

    Init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CBG::Application.config.secret_token = '6e23378e5f600425477667ef5f9219dc97ef63bfa39c6c3cbb2774e3be0594859c2c39fb4355eea3d4f6c9211ab88a3b271172f2c2516a85ed3e3a7d5bfb10ab'
